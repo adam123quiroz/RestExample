a2ee3d0a07917b13e0c1deab0e47b19c41ec689d
Adam Hans Quiroz Castillo <adam123quiroz@gmail.com>
2019-09-12 08:28:12 -0400

    arreglando errores

--- src/main/java/com/software3/Rest/LoadDatabase.java
+++ src/main/java/com/software3/Rest/LoadDatabase.java
@@ -11,10 +11,16 @@ import org.springframework.context.annotation.Configuration;
 class LoadDatabase {
 
     @Bean
-    CommandLineRunner initDatabase(EmployeeRepository repository) {
+    CommandLineRunner initDatabase(EmployeeRepository repository, OrderRepository orderRepository) {
         return args -> {
-            log.info("Preloading " + repository.save(new Employee("Bilbo Baggins", "burglar")));
-            log.info("Preloading " + repository.save(new Employee("Frodo Baggins", "thief")));
+            log.info("Preloading " + repository.save(new Employee("Bilbo Baggins", "burglar", "Tecnico")));
+            log.info("Preloading " + repository.save(new Employee("Frodo Baggins", "thief", "Ingeniero")));
+            orderRepository.save(new Order("MacBook Pro", Status.COMPLETED));
+            orderRepository.save(new Order("iPhone", Status.IN_PROGRESS));
+
+            orderRepository.findAll().forEach(order -> {
+                log.info("Preloaded " + order);
+            });
         };
     }
 }
